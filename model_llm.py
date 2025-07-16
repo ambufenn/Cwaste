a51c560c70486132e0430c2ccacfbdbe928aada9
fenira that't it <108204426+ambufenn@users.noreply.github.com>
2025-07-16 13:22:44 +0700

    Update model_llm.py

--- model_llm.py
+++ model_llm.py
@@ -1,10 +1,10 @@
 from transformers import pipeline
 
-# Inisialisasi pipeline LLM dari Hugging Face
+# Inisialisasi pipeline dengan model dari Hugging Face
 pipe = pipeline(
     "text-generation",
-    model="HuggingFaceH4/zephyr-7b-beta",
-    max_new_tokens=128,
+    model="satvikag/chatbot",
+    max_new_tokens=100,
     do_sample=True,
     temperature=0.7,
 )
