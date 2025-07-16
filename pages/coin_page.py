390d70ee40f01ef73730191c082faeaacd081306
fenira that't it <108204426+ambufenn@users.noreply.github.com>
2025-07-16 16:16:37 +0700

    Create coin_page.py

---
+++ pages/coin_page.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def run():
+    st.header("💰 e-Wallet & Investasi")
+    st.info("Saldo e-Wallet dan fitur investasi akan segera hadir!")
+    st.markdown("- Saldo saat ini: `Rp 0`")
+    st.markdown("- Portofolio investasi: _coming soon_")
