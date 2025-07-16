7390669d561d8c2c936062cfc3d5681f0a3128c3
fenira that't it <108204426+ambufenn@users.noreply.github.com>
2025-07-16 10:37:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+from model_vlm import classify_image_from_file
+import tempfile
+from PIL import Image
+
+hf_token = st.secrets["HF_TOKEN"]
+
+uploaded_file = st.file_uploader("Upload gambar")
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".png") as temp:
+        image.save(temp.name)
+        result = classify_image_from_file(temp.name, token=hf_token)
+    st.write(f"Hasil klasifikasi: {result}")
 
